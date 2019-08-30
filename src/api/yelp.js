d8ebc85527ba1d2d6e0482ebcf6adfc63d40a4f9
rofirahman <rofirahman@icloud.com>
2019-08-30 19:47:32 +0800

    Initial Commit

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import Config from './Config';
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer 2e4oYm9tdXwPDpUfTJrnx-pXKlabvhapWvglg3gX5wFN4HxoLofU03HdbNgIAE09NIOAPsFUw19B7twNr_owHycN7gTEWlfiAhgipeuTW_yWyirC7N6l0X3uIUhnXXYx'
+        Authorization: `Bearer ${Config.YELP_API_KEY}`
     }
 })
